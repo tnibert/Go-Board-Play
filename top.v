69832db77184b44d054a8b9f2582c17d5b2e8138
Timothy Nibert <docz2a@gmail.com>
2025-01-14 21:48:59 +1100

    store led states in registers to toggle on off with rising edge of clock

--- top.v
+++ top.v
@@ -1,4 +1,5 @@
 module top(
+    input i_Clk,
     input i_Switch_1,
     input i_Switch_2,
     input i_Switch_3,
@@ -6,10 +7,44 @@ module top(
     output o_LED_1,
     output o_LED_2,
     output o_LED_3,
-    output o_LED_4);
+    output o_LED_4,
+);
+    reg r_LED_1    = 1'b0;
+    reg r_Switch_1 = 1'b0;
+    reg r_LED_2    = 1'b0;
+    reg r_Switch_2 = 1'b0;
+    reg r_LED_3    = 1'b0;
+    reg r_Switch_3 = 1'b0;
+    reg r_LED_4    = 1'b0;
+    reg r_Switch_4 = 1'b0;
 
-    assign o_LED_1 = i_Switch_1;
-    assign o_LED_2 = i_Switch_2;
-    assign o_LED_3 = i_Switch_3;
-    assign o_LED_4 = i_Switch_4;
+    always @(posedge i_Clk)
+    begin
+      r_Switch_1 <= i_Switch_1;
+      r_Switch_2 <= i_Switch_2;
+      r_Switch_3 <= i_Switch_3;
+      r_Switch_4 <= i_Switch_4;
+
+      if (i_Switch_1 == 1'b0 && r_Switch_1 == 1'b1)
+      begin
+        r_LED_1 <= ~r_LED_1;
+      end
+      if (i_Switch_2 == 1'b0 && r_Switch_2 == 1'b1)
+      begin
+        r_LED_2 <= ~r_LED_2;
+      end
+      if (i_Switch_3 == 1'b0 && r_Switch_3 == 1'b1)
+      begin
+        r_LED_3 <= ~r_LED_3;
+      end
+      if (i_Switch_4 == 1'b0 && r_Switch_4 == 1'b1)
+      begin
+        r_LED_4 <= ~r_LED_4;
+      end
+    end
+
+    assign o_LED_1 = r_LED_1;
+    assign o_LED_2 = r_LED_2;
+    assign o_LED_3 = r_LED_3;
+    assign o_LED_4 = r_LED_4;
 endmodule
